e2d538f927e0346676d71c314c3e356bc83477e3
Joe <totenlicht@mail.ru>
2022-09-30 10:24:06 +0500

    Solved task 1 from lesson 09 (part 2)
    Задания по теме "Администрирование MySQL"

---
+++ lesson_09/task_1-4_09_part2.sql
@@ -0,0 +1,9 @@
+-- ПЗ 1. Создайте двух пользователей, которые имеют доступ к базе данных shop. 
+-- Первому пользователю shop_read должны быть доступны только запросы на чтение данных, 
+-- второму пользователю shop — любые операции в пределах базы данных shop.
+
+CREATE USER shop_read;
+GRANT SELECT ON *.* TO shop_read;
+
+CREATE USER shop;
+GRANT ALL ON shop.* TO shop;
